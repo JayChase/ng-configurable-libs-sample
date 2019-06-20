6862ac946d1bd6dd80d612226265159500fdfcad
JayChase <JonathanChase@outlook.com>
2019-06-20 12:10:21 +0900

    typed token

--- projects/normal-lib/src/lib/normal-lib-config.token.ts
+++ projects/normal-lib/src/lib/normal-lib-config.token.ts
@@ -1,3 +1,6 @@
 import { InjectionToken } from '@angular/core';
+import { NormalLibConfig } from './normal-lib-config';
 
-export const NORMAL_LIB_CONFIG = new InjectionToken('NORMAL_LIB_CONFIG');
+export const NORMAL_LIB_CONFIG = new InjectionToken<NormalLibConfig>(
+  'NORMAL_LIB_CONFIG'
+);
